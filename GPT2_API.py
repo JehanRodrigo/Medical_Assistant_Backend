2b29df5cd30f87193cd1392fea9785f1752f5e5b
Ashali Samaraweera <ashalisanjula@gmail.com>
2024-11-22 12:57:51 +0530

    using apikey

---
+++ GPT2_API.py
@@ -0,0 +1,23 @@
+import requests
+
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+API_KEY= os.getenv("GPT2_API2")
+
+API_URL = "https://api-inference.huggingface.co/models/openai-community/gpt2"
+headers = {"Authorization": f"Bearer {API_KEY}"}
+
+# headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+	
+output = query({
+	"inputs": "Can you please let us know more details about your ",
+})
+
+print(output)
