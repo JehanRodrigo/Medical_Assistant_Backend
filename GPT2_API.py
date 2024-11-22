4a5ef79d7cee2546ff9f2ee8bd6cd71c9caed392
Ashali Samaraweera <ashalisanjula@gmail.com>
2024-11-22 13:06:08 +0530

    direct api call

--- GPT2_API.py
+++ GPT2_API.py
@@ -5,13 +5,12 @@ import os
 
 
 load_dotenv()
-API_KEY= os.getenv("GPT2_API2")
+# API_KEY= os.getenv("GPT2_API2")
 
 API_URL = "https://api-inference.huggingface.co/models/openai-community/gpt2"
-headers = {"Authorization": f"Bearer {API_KEY}"}
+# headers = {"Authorization": f"Bearer {API_KEY}"}
 
-# headers = {"Authorization": "Bearer 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"}
-
+headers = {"Authorization": "Bearer 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"}
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
--- medi_backend_pipe.py
+++ medi_backend_pipe.py
@@ -48,11 +48,11 @@ import os
 
 # Load environment variables
 load_dotenv()
-API_KEY = os.getenv("GPT2_API2")
+# API_KEY = os.getenv("GPT2_API2")
 
 # Hugging Face API configuration
 API_URL = "https://api-inference.huggingface.co/models/openai-community/gpt2"
-headers = {"Authorization": f"Bearer {API_KEY}"}
+headers = {"Authorization": "Bearer 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"}
 
 def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
