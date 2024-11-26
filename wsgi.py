b84c2a574e40cffc6426436f9ae6d10b4dd66bd9
JehanRodrigo <jehanrodrigo31@gmail.com>
2024-11-26 11:58:19 +0530

    Pull Req Revert

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 from medi_backend_pipe import app
+=======
+from medi_backend import app
+>>>>>>> parent of f28773e (Dummy Data  Backend removed)
 
 if __name__ == "__main__":
     app.run()
